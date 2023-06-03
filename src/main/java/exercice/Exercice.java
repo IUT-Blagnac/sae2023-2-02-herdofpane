3f649bdf063435cd3567e1d03a9c02a7e2fce596
herdofpane <121870569+herdofpane@users.noreply.github.com>
2023-06-03 19:02:08 +0200

    code

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -9,14 +9,28 @@ public class Exercice {
     static ArrayList<String> listefinale = new ArrayList<>();
 
     public static List<String> solution(String str, List<Character> ordre) {
-        // Permet de separer la Sring en plusieur partie pour recuperer chaque mots
-        // Les partie a separer sont délimité par les espace et les apostrophes
-        StringTokenizer tokenizer = new StringTokenizer(str, " ;’");
+        listefinale.clear();
+        // Verifie que la chaine n'est pas vide
+        if (str.isEmpty()) {
+            List<String> list = listefinale;
+            return list;
+        }
 
+        // Permet de separer la Sring en plusieur partie pour recuperer chaque mots
+        // Les partie a separer sont délimité par des espace, des apostrophes, ect...
+        StringTokenizer tokenizer = new StringTokenizer(str, " ;’;, ;'");
+
+        // Premet de rajouter chaque mots dans une ArrayList
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken();
             tokens.add(token);
         }
+        // Verifie que la chaine ne contient pas qu'un seul mots
+        if (tokens.size() == 1) {
+            listefinale.add("" + str);
+            return listefinale;
+        }
+
         trie(tokens, ordre);
         return listefinale;
     }
--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String texte = "Il fait beau aujourd’hui comme en aout";
+        String texte = "";
         List<Character> ordre = List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o');
         System.out.println(Exercice.solution(texte, ordre));
         // output: [classer, texte, exemple, a, de]
